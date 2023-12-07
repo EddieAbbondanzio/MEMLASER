f6a47b51e0bce8d50e3b1c6c3369b18a829c3255
Ed Abbondanzio <me@eddieabbondanz.io>
2023-12-07 00:33:43 -0500

    Add sass

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
+import "./index.sass";
 
-const root = createRoot(document.body);
+const root = createRoot(document.getElementById("root"));
 root.render(<h2>Hello from React!</h2>);
