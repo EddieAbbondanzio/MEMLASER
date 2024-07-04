295d5cee0a788073d49ab9ed561a5ab039794109
Ed Abbondanzio <me@eddieabbondanz.io>
2024-07-04 00:46:17 -0400

    Add file picker test

--- app/lib/src/editor/sidebar.dart
+++ app/lib/src/editor/sidebar.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -9,8 +12,24 @@ class Sidebar extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(8.0),
       width: 320.0,
-      child: const Column(
-        children: [Text("Sidebar")],
+      color: Colors.white70,
+      child: Column(
+        children: [
+          const Text("Sidebar"),
+          IconButton(
+              onPressed: () async {
+                print("CLICK!");
+                FilePickerResult? result =
+                    await FilePicker.platform.pickFiles();
+
+                if (result != null) {
+                  File file = File(result.files.single.path!);
+                  print('File: ${file.path}');
+                }
+              },
+              color: Colors.amber,
+              icon: const Icon(Icons.add))
+        ],
       ),
     );
   }
