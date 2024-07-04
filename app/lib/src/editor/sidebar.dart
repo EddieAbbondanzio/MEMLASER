fce6bfd12c293db7489ef402a070c671992e4679
Ed Abbondanzio <me@eddieabbondanz.io>
2024-07-04 00:35:03 -0400

    Test out sidebar

--- app/lib/src/editor/sidebar.dart
+++ app/lib/src/editor/sidebar.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Sidebar extends StatelessWidget {
   const Sidebar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      children: [Text("sidebar")],
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      width: 320.0,
+      child: const Column(
+        children: [Text("Sidebar")],
+      ),
     );
   }
 }
