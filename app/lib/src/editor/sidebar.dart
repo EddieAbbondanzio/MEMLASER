162acc7546e63c022428a05c7d2104ee067d7b86
Ed Abbondanzio <me@eddieabbondanz.io>
2024-07-05 00:01:53 -0400

    Improve sidebar design to look like something usable

--- app/lib/src/editor/sidebar.dart
+++ app/lib/src/editor/sidebar.dart
@@ -10,25 +10,46 @@ class Sidebar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(12.0),
       width: 320.0,
-      color: Colors.white70,
+      color: Colors.white,
       child: Column(
         children: [
-          const Text("Sidebar"),
-          IconButton(
-              onPressed: () async {
-                print("CLICK!");
-                FilePickerResult? result =
-                    await FilePicker.platform.pickFiles();
+          Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Snapshots",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize:
+                          Theme.of(context).textTheme.bodyLarge?.fontSize),
+                ),
+                TextButton(
+                    style:
+                        TextButton.styleFrom(backgroundColor: Colors.black12),
+                    onPressed: () async {
+                      print("CLICK!");
+                      FilePickerResult? result =
+                          await FilePicker.platform.pickFiles();
 
-                if (result != null) {
-                  File file = File(result.files.single.path!);
-                  print('File: ${file.path}');
-                }
-              },
-              color: Colors.amber,
-              icon: const Icon(Icons.add))
+                      if (result != null) {
+                        File file = File(result.files.single.path!);
+                        print('File: ${file.path}');
+
+                        // Now we'd make an HTTP request to API and give it the path.
+                        // POST snapshot/import {
+                        //  file: file.path
+                        // }
+                      }
+                    },
+                    child: const Text(
+                      "Import",
+                      style: TextStyle(color: Colors.black),
+                    ))
+              ])
         ],
       ),
     );
