0126adec0cbcd4f7c7208f82b93543ffe6117dba
Ed Abbondanzio <me@eddieabbondanz.io>
2024-07-12 20:55:54 -0400

    Move ports to config

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,12 +4,14 @@ dotenv.config();
 import { NestFactory } from "@nestjs/core";
 import { EditorModule } from "./editor/editor.module.js";
 import { ValidationPipe } from "@nestjs/common";
+import { HTTP_PORT } from "./core/config.js";
+import { CoreModule } from "./core/core.module.js";
 
 async function bootstrap() {
-  const app = await NestFactory.create(EditorModule);
+  const app = await NestFactory.create([CoreModule, EditorModule]);
   // Validates incoming JSON to classes.
   app.useGlobalPipes(new ValidationPipe());
   console.log("node env 2: ", process.env.NODE_ENV);
-  await app.listen(3000);
+  await app.listen(HTTP_PORT);
 }
 bootstrap();
